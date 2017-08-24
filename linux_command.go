9822867112123d3eb9e400e2dfb52bcfbf862ecb
root <root@vm-sst-noops-jira-66-23.tuniu.org>
2017-08-24 18:02:17 +0800

    go调用linux命令测试

--- linux_command.go
+++ linux_command.go
@@ -10,11 +10,27 @@ import (
 var p = fmt.Println
 
 func main() {
+    if runtime.GOOS == "linux"{
+        p("----------------------------------------------------------")
+        p("Env: linux")
+        p("----------------------------------------------------------")
+        p()
+    }
+
     if runtime.GOOS == "linux" {
-        s, err := exec.Command("/usr/sbin/dmidecode", "--string", "system-serial-number").Output()        
-        if err != nil {
-            p(err)
+        sn, err_1 := exec.Command("/usr/sbin/dmidecode", "--string", "system-serial-number").Output()        
+        ls, err_2 := exec.Command("/usr/bin/ls").Output()        
+        if err_1 != nil {
+            p(err_1)
         }
-        p(string(s))
+        p("Serial Number:",string(sn))
+        p("----------------------------------------------------------")
+        p()
+        if err_2 != nil {
+            p(err_2)
+        }
+        p("files in current dir:")
+        p("----------------------------------------------------------")
+        p(string(ls))
     }
 }
